2b9c2e04b8514df07f22ade6c897d4183cd73995
hendurhance <50846992+hendurhance@users.noreply.github.com>
2020-12-14 22:53:28 +0100

    first push to git

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <ul>
+    <elearning-resources 
+    v-for="res in storedResources" 
+    :key="res.id" 
+    :title="res.title"
+    :description="res.description"
+    :link="res.link"
+    ></elearning-resources>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import eLearningResouces from './components/elearning-resources/eResourceItem.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    'elearning-resources': eLearningResouces
+  },
+  data(){
+    return{
+      storedResources: [
+        { 
+          id: 'official-guide', 
+          title: 'Official Guide', 
+          description: 'The official Vue.js development guide', 
+          link: 'https://vuejs.org'
+        },
+        { 
+          id: 'google', 
+          title: 'Google', 
+          description: 'Google search engine', 
+          link: 'https://google.com'
+        },
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
 }
+
+
 </style>
